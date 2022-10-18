7d674bc16ffe095f4a576148cbfafb14c3cc2e14
angelo59930 <manfrediangelosantiago10@gmail.com>
2022-10-18 16:42:30 -0300

    añadido viewBinding

--- app/src/main/java/com/iua/elcarrito/MainActivity.kt
+++ app/src/main/java/com/iua/elcarrito/MainActivity.kt
@@ -3,14 +3,19 @@ package com.iua.elcarrito
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.iua.elcarrito.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+  private lateinit var binding:ActivityMainBinding
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    binding = ActivityMainBinding.inflate(layoutInflater)
+    val view = binding.root
+    setContentView(view)
 
 
-    Log.d("mensaje","test")
 
   }
 }
