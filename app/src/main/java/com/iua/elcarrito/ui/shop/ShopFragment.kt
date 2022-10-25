55658e047ffbac3be886e7246898e0f73bd05805
angelo59930 <manfrediangelosantiago10@gmail.com>
2022-10-25 14:53:03 -0300

    terminadas mas pantallas

--- app/src/main/java/com/iua/elcarrito/ui/shop/ShopFragment.kt
+++ app/src/main/java/com/iua/elcarrito/ui/shop/ShopFragment.kt
@@ -1,13 +1,20 @@
 package com.iua.elcarrito.ui.shop
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.iua.elcarrito.R
+import com.iua.elcarrito.databinding.FragmentShopBinding
 
 class ShopFragment : Fragment() {
+
+  private var _binding: FragmentShopBinding? = null
+  private val binding get() = _binding!!
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -18,6 +25,17 @@ class ShopFragment : Fragment() {
     inflater: LayoutInflater, container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View? {
+    _binding = FragmentShopBinding.inflate(inflater, container, false)
+
+    binding.compra.setOnClickListener {
+      Log.d("BOTON","PRESIONE EL BOTON COMPRAR")
+      findNavController().navigate(R.id.action_nav_shop_to_orderFragment)
+    }
+
+    binding.volver.setOnClickListener {
+      findNavController().navigate(R.id.action_nav_shop_to_nav_home)
+    }
+
     return inflater.inflate(R.layout.fragment_shop, container, false)
   }
 
