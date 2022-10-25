c84254425fa7d592949c7e7c307c0bd7b4aa6bd1
FatiVenturino <53906016+FatiVenturino@users.noreply.github.com>
2022-10-25 17:46:32 -0300

    Add files via upload

--- app/src/main/java/com/iua/elcarrito/ui/profile/EditProfileFragment.kt
+++ app/src/main/java/com/iua/elcarrito/ui/profile/EditProfileFragment.kt
@@ -7,29 +7,29 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.iua.elcarrito.R
-import com.iua.elcarrito.databinding.FragmentProfileBinding
+import com.iua.elcarrito.databinding.FragmentEditProfileBinding
 
-class ProfileFragment : Fragment() {
+class EditProfileFragment : Fragment() {
 
-  private lateinit var binding: FragmentProfileBinding
+  private lateinit var binding: FragmentEditProfileBinding
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
   }
 
   override fun onCreateView(
-    inflater: LayoutInflater, container: ViewGroup?,
+    inflater: LayoutInflater,
+    container: ViewGroup?,
     savedInstanceState: Bundle?
-  ): View? {
-    binding = FragmentProfileBinding.inflate(inflater,container,false)
-    return inflater.inflate(R.layout.fragment_profile, container, false)
+  ): View {
+    binding = FragmentEditProfileBinding.inflate(inflater,container,false)
+    return binding.root
   }
-
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
-    binding.editProfile.setOnClickListener {
-      findNavController().navigate(R.id.action_nav_profile_to_editProfileFragment)
+    binding.completeEdit.setOnClickListener {
+      findNavController().navigate(R.id.action_editProfileFragment_to_nav_profile)
     }
   }
 
